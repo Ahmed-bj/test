0721e9f4aedd73596d85c69fa65ef053013defdb
Ahmed BEN JEMAA <benjemaaa344@gmail.com>
2023-06-21 21:11:01 +0200

    add project

---
+++ src/main/java/com/project/GestionPersonne/GestionPersonneApplication.java
@@ -0,0 +1,13 @@
+package com.project.GestionPersonne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPersonneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionPersonneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/GestionPersonne/GestionPersonneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.GestionPersonne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPersonneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
