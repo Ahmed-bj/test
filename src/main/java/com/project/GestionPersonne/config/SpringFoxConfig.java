825af252f6b185dd485f182339fa20e0eb04476f
Ahmed BEN JEMAA <benjemaaa344@gmail.com>
2023-06-21 23:14:28 +0200

    add postman collection

--- src/main/java/com/project/GestionPersonne/config/SpringFoxConfig.java
+++ src/main/java/com/project/GestionPersonne/config/SpringFoxConfig.java
@@ -1,5 +1,17 @@
 package com.project.GestionPersonne.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.builders.RequestHandlerSelectors;
+
+
+
 public class SpringFoxConfig {
 
 }
